dcf9f625458a4f34a625f8867fea374fd8361ff5
Megan H <megannh6349@gmail.com>
2020-07-13 01:10:45 -0400

    finished controllers folder

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,35 @@
+// Import express
+var express = require("express");
+
+// Import burger.js
+var burger = require("../models/burger.js");
+
+// Create routes and set up logic within those routes where required
+router.get("/", function (req, res) {
+    cat.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function (req, res) {
+    burger.create(["burger_name"], [req.body.name], function (data) {
+        res.redirect("/");
+    });
+});
+
+router.put("/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({ devoured: req.body.devoured }, condition, function () {
+        res.redirect("/");
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
