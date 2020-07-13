4fb8634f628a1b57082eace2714d7e8b66520d6c
Megan H <megannh6349@gmail.com>
2020-07-12 21:35:15 -0400

    created schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Burger1');
+INSERT INTO burgers (name) VALUES ('Burger2');
+INSERT INTO burgers (name) VALUES ('Burger3');
+INSERT INTO burgers (name) VALUES ('Burger4');
