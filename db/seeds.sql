d109fb42c7b2153d2eef7268fad6c560eb986c9b
Megan H <megannh6349@gmail.com>
2020-07-13 00:24:30 -0400

    fixed bug - burger_name

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (name) VALUES ('Burger1');
-INSERT INTO burgers (name) VALUES ('Burger2');
-INSERT INTO burgers (name) VALUES ('Burger3');
-INSERT INTO burgers (name) VALUES ('Burger4');
+INSERT INTO burgers (burger_name) VALUES ('Burger1');
+INSERT INTO burgers (burger_name) VALUES ('Burger2');
+INSERT INTO burgers (burger_name) VALUES ('Burger3');
+INSERT INTO burgers (burger_name) VALUES ('Burger4');
